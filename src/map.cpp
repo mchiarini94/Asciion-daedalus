e914bd2517c4d02edec044d09079539da329a466
hyspxt <secondopatto@gmail.com>
2022-07-23 18:38:42 +0200

    added the map prototype

--- src/map.cpp
+++ src/map.cpp
@@ -3,3 +3,26 @@
 
 #include <ncurses.h>
 #include <unistd.h>
+
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Definizione di costanti
+int main()
+{
+    initscr();
+    for (int cordX = 1; cordX < 20; cordX++){
+        
+        mvprintw(1, cordX, "h");
+        sleep(1);
+        refresh();
+
+    }
+    endwin();
+    return (0);
+    }
